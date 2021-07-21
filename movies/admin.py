b7756da9ed2df8bc39f56a0b0c19082fb6a165b0
nikita <nikit0zpart0z@gmail.com>
2021-07-21 22:27:09 +0300

    Registered models at django admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Genre, Actor, Country, Movie, Review
+
+admin.site.register(Category)
+admin.site.register(Genre)
+admin.site.register(Actor)
+admin.site.register(Country)
+admin.site.register(Movie)
+admin.site.register(Review)
