2a86acad690343b10cbfbb7f27a32eeca6d63a03
nikita <nikit0zpart0z@gmail.com>
2021-08-28 20:46:10 +0300

    Added urls for like and dislike movie

--- movies/urls.py
+++ movies/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from movies.views import ViewMovies, ViewMovie
+from movies.views import ViewMovies, ViewMovie, AddLikeToMovie, AddDislikeToMovie
 
 
 urlpatterns = [
-    path('', ViewMovies.as_view(), name='view_movies'),
+    path('', ViewMovies.as_view(), name='view-movies'),
     path('view-movie/<slug:movie_slug>', ViewMovie.as_view(), name='view-movie'),
+    path('like-movie/<int:movie_pk>', AddLikeToMovie.as_view(), name='like-movie'),
+    path('dislike-movie/<int:movie_pk>', AddDislikeToMovie.as_view(), name='dislike-movie'),
 ]
